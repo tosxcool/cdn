ed92691bcbb909f0796859557116900cfecc80ca
tosxcool <khrutos@gmail.com>
2024-10-12 21:36:02 +0700

    Create fb.js

---
+++ fb.js
@@ -0,0 +1,3 @@
+function setFooter(){
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/schoolnamwing" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">คุณครูจักรกฤษ มาโต &copy; 2024 <a style="color:gray;text-decoration:none" href="https://www.facebook.com/Khrutos" target="_blank"> Contact</a></center>'
+  }
